04ab5da4fa721d88637e6c308a87f6f248e6d1c2
ianriva <ianriva96.ir@gmail.com>
2020-08-27 20:08:56 -0300

    TP1-Ejercicio3

--- tp1/Ej3.cs
+++ tp1/Ej3.cs
@@ -5,8 +5,22 @@ using System;
 
 public class Ej3
 {
+    public static bool esPar(int numero)
+    {
+        if (numero % 2 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
     public static void Ejercicio3()
     {
-
+        int num;
+        Console.WriteLine("Ingrese un número para saber si es par:");
+        num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(esPar(num));
     }
 }
